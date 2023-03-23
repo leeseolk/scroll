243d44357e584365f8ee59ca4247d75043242b51
seol <116864713+leeseolk@users.noreply.github.com>
2023-03-23 15:29:52 +0900

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,44 @@
+window.onload = function(){
+    var section = document.getElementsByTagName("section");
+    var pointBtn = document.querySelectorAll(".pointWrap li");
+    var pageNum = 0;
+    var totalNum = section.length;
+
+    for( var i = 0; i < pointBtn.length; i++ ){
+      (function(idx) {
+          pointBtn[idx].onclick = function() {
+              pageNum = idx;
+              pageChangeFunc();
+              window.scrollTo({
+                top: section[pageNum].offsetTop,
+                behavior: 'smooth',
+              })
+          }
+      })(i);
+  }
+  
+        window.addEventListener("scroll", function(event){
+            var scroll = this.scrollY;
+  
+            for(var i=0; i<totalNum; i++){
+              if(scroll > section[i].offsetTop - window.outerHeight/1.5  && scroll < section[i].offsetTop - window.outerHeight/1.5 + section[i].offsetHeight){
+                pageNum = i;
+                break;
+              }
+            }
+            pageChangeFunc();
+          });
+  
+  
+          function pageChangeFunc(){
+            for(var i=0; i<totalNum; i++){
+              section[i].classList.remove("active");
+              pointBtn[i].classList.remove("active");
+            }
+            section[pageNum].classList.add("active");
+            pointBtn[pageNum].classList.add("active");
+            
+          }
+  
+          pageChangeFunc();
+        }
